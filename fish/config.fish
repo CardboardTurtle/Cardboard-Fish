baed5e87e73ac4992e96f722981e90ef2b78a00d
CardboardTurtle <43497630+CardboardTurtle@users.noreply.github.com>
2020-11-15 16:01:50 -0600

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+clear
+alias tetris "vitetris"
+alias install "sudo apt install"
+alias remove "sudo apt remove"
+alias purge "sudo apt purge"
+alias minesweeper "freesweep"
+alias home "cd ~"
+alias starwars "telnet towel.blinkenlights.nl"
+alias rofi "rofi -show run"
+echo "Cardboard Turtle's Fish Shell"
+echo " "
+neofetch
