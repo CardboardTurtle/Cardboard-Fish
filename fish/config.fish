46572c0ab5bd733ee36ab5cf57d4b9d817e3fcf7
CardboardTurtle <43497630+CardboardTurtle@users.noreply.github.com>
2020-11-19 18:20:06 -0600

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,14 @@
+clear
+cd ~
 alias tetris "vitetris"
 alias install "sudo apt install"
 alias remove "sudo apt remove"
 alias purge "sudo apt purge"
 alias minesweeper "freesweep"
 alias home "cd ~"
+alias back "cd -"
+alias up "cd .."
+alias cls "clear"
 alias documents "cd ~/Documents"
 alias pictures "cd ~/Pictures"
 alias videos "cd ~/Videos"
@@ -12,6 +17,28 @@ alias music "cd ~/Music"
 alias desktop "cd ~/Desktop"
 alias starwars "telnet towel.blinkenlights.nl"
 alias rofi "rofi -show run"
-echo (set_color brgreen) "Cardboard Turtle's Fish Shell"
-echo " "
-neofetch
+alias coronachan "curl https://corona-stats.online/states/us"
+alias secondary "cd /;cd /secondary"
+alias tertiary "cd /;cd /tertiary"
+alias primary "cd /"
+alias cardboardturtle 'Github: https://github.com/CardboardTurtle Ruqqus: https://ruqqus.com/@anaggravatedturtle  Matrix.org: @cardboard_turtle:matrix.org'
+echo -e  (set_color brcyan) '__________________________________________
+< Welcome to Cardboard Turtle`s Fish Shell! >
+ ------------------------------------------
+    \    '(set_color brgreen)'                              ___-------___
+ '(set_color brcyan)'    \  '(set_color brgreen)'                           _-~~             ~~-_
+  '(set_color brcyan)'    \       '(set_color brgreen)'                  _-~                    /~-_
+             /^\__/^\         /~  \                   /    \
+           /|  O|| O|        /      \_______________/        \
+          | |___||__|      /       /                \          \
+          |          \    /      /                    \          \
+          |   (_______) /______/                        \_________ \
+          |         / /         \                      /            \
+           \         \^\\         \                  /               \     /
+             \         ||           \______________/      _-_       //\__//
+               \       ||------_-~~-_ ------------- \ --/~   ~\    || __/
+                 ~-----||====/~     |==================|       |/~~~~~
+                  (_(__/  ./     /                    \_\      \.
+                         (_(___/                         \_____)_) '
+echo (set_color brcyan) ''
+date +"Today is %A, %B %d" | figlet -f mini
