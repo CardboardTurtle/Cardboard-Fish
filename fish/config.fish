0ff4b7a60ba7107ffd0661b4488bc4f56d859d7e
CardboardTurtle <43497630+CardboardTurtle@users.noreply.github.com>
2020-11-26 00:11:50 -0600

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,6 @@
 clear
 cd ~
-function vlc --wraps=vlc
-    command vlc $argv >/dev/null 2>&1 &
-    disown
-end
-funcsave vlc
+alias penis "figlet -f mini 8========D | lolcat -F 0.5;echo hahahaha funny genitals haha"
 abbr pm "vlc ~/Music"
 abbr tetris "vitetris"
 abbr install "sudo apt install"
@@ -35,10 +31,14 @@ abbr root "cd /"
 abbr config "micro ~/.config/fish/config.fish"
 abbr cfg "micro ~/.config/fish/config.fish"
 abbr colors "micro ~/.config/fish/fish_variables"
-abbr cardboardturtle 'Github: https://github.com/CardboardTurtle Ruqqus: https://ruqqus.com/@anaggravatedturtle  Matrix.org: @cardboard_turtle:matrix.org'
-echo -e  (set_color brgreen) '___________________________________________
-| Welcome to Cardboard Turtle`s Fish Shell! |
- -------------------------------------------
+abbr cardboardturtle 'Github: https://github.com/CardboardTurtle Ruqqus: https://ruqqus.com/@anaggravatedturtle Matrix.org: @cardboard_turtle:matrix.org Steam: https://steamcommunity.com/id/CardboardTurtle/'
+abbr readme "micro .config/fish/readme.txt"
+abbr pfetch ~/.config/fish/pfetch
+abbr clock "tty-clock -c -t -d 0.1 -S -s" 
+alias dir "ls"
+echo -e  (set_color brgreen) '_________________________________________
+|Welcome to CardboardTurtle`s Fish Shell! |
+ -----------------------------------------
     \    '(set_color ad8762)'                              ___-------___
  '(set_color brgreen)'    \  '(set_color a77f58)'                           _-~~             ~~-_
   '(set_color brgreen)'    \       '(set_color a77f58)'                  _-~                   / ~-_
@@ -54,6 +54,5 @@ echo -e  (set_color brgreen) '___________________________________________
                  ~-----||====/~     |==================|       |/~~~~~
                   (_(__/  ./     /                    \_\      \.
                          (_(___/                         \_____)_) '
-printf (set_color brcyan)
-date +"Today is %A, %B %d." | figlet -f mini
-date +"The time is %l:%M %p" | figlet -f mini
+printf (set_color brcyan) ""
+date +"%a, %b. %d. %l:%M" |figlet -f small
